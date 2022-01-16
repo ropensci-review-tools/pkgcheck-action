05233df660c498fb8f7b2f5d6427d09d80c44b3a
assignUser <jacob@wujciak.de>
2022-01-16 20:39:06 +0100

    add newline

--- check.R
+++ check.R
@@ -3,7 +3,11 @@ library(magrittr)
 
 check <- pkgcheck()
 print(check$info$network_file)
-paste0("::set-output name=visnet_path::", check$info$network_file) %>% cat()
+paste0(
+    "::set-output name=visnet_path::",
+    check$info$network_file, 
+    "\n"
+) %>% cat()
 
 md <- checks_to_markdown(check)
 writeLines(md, "pkgcheck-results.md")
@@ -11,12 +15,13 @@ rmarkdown::render("pkgcheck-results.md", quiet = TRUE)
 
 errors <- grep(":heavy_multiplication_x:", md) %>%
     `[`(md, .) %>%
-    gsub("- :heavy_multiplication_x:", "::error ::", .)
+    gsub("- :heavy_multiplication_x:", "::error ::", .) %>%
+    paste0("\n")
 
 for (error in errors) {
     cat(error)
 }
 
-cat("::group::Check Results")
+cat("::group::Check Results\n")
 check
-cat("::endgroup::")
+cat("::endgroup::\n")
