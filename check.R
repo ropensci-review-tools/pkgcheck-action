9d573af3ca44ddba8c5fb47a2d8280661b71c384
assignUser <jacob@wujciak.de>
2022-01-16 20:10:59 +0100

    fdebug output

--- check.R
+++ check.R
@@ -2,11 +2,12 @@ library(pkgcheck)
 library(magrittr)
 
 check <- pkgcheck()
+print(check$info$network_file)
 paste0("::set-output name=visnet_path::", check$info$network_file) %>% cat()
 
 md <- checks_to_markdown(check)
 writeLines(md, "pkgcheck-results.md")
-rmarkdown::render("pkgcheck-results.md")
+rmarkdown::render("pkgcheck-results.md", quiet = TRUE)
 
 errors <- grep(":heavy_multiplication_x:", md) %>%
     `[`(md, .) %>%
